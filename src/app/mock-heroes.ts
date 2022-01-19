78b313ec5343dc13a08f0aba65706eb50e330c9b
Fernanda Marinho <Nandamarin@users.noreply.github.com>
2022-01-19 17:17:22 -0300

    Criada lista de heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Batman' },
+  { id: 2, name: 'Homem aranha' },
+  { id: 3, name: 'Wolverine' },
+  { id: 4, name: 'Pantera-Negra' },
+  { id: 5, name: 'Mulher Maravilha' },
+  { id: 6, name: 'Capitã Marvel' },
+  { id: 7, name: 'Super Homem' },
+  { id: 8, name: 'Hulk' },
+  { id: 9, name: 'Homem de Ferro' },
+  { id: 10, name: 'Lanterna Verde' },
+  { id: 11, name: 'Capitão América' },
+  { id: 12, name: 'Mulher Gato' },
+];
